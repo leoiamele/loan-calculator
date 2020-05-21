d71225d7eed8f17aa09572af0e2f07253f9612eb
Leandro Iamele <leandroiamele@gmail.com>
2020-05-21 09:32:52 -0300

    Process form information

---
+++ app.js
@@ -0,0 +1,8 @@
+//Listen to Calculate button
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+//calculateResults Function
+function calculateResults(e){
+  console.log('Calculating...');
+  e.preventDefault();
+}
